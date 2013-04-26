94ab704adaa16b36585b89e0e989d7394c5f1375
dbrodhead <daniel.brodhead@outlook.com>
2013-04-26 14:05:07 +0000

    initializing git

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+ResTest2::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ResTest2::Application.config.secret_token = '535dd0dc1ead50c2b87e5b6d33deef121311dee1c4c17ffd306fcf6cb67157d2a378b63dff0c9aa822599aaf6790500b10208126a4cafb90529f535d3be7ab71'
