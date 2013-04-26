d719a80fe5c5cef836724ab9047046e9b068f83b
dbrodhead <daniel.brodhead@outlook.com>
2013-04-26 19:04:08 +0000

    annotated Period,TCalDay

--- test/unit/period_test.rb
+++ test/unit/period_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: periods
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  number     :integer
+#  stime      :time
+#  etime      :time
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class PeriodTest < ActiveSupport::TestCase
--- test/unit/t_cal_day_test.rb
+++ test/unit/t_cal_day_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: t_cal_days
+#
+#  id         :integer          not null, primary key
+#  date       :date
+#  details    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class TCalDayTest < ActiveSupport::TestCase
